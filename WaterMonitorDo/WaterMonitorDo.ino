7cfae3a3de839b2114a476a7ec188beaac0890df
rtourte@yahoo.fr <_Corwin12>
2019-09-23 09:34:48 +0200

    Add comments & general informations

--- WaterMonitorDo/WaterMonitorDo.ino
+++ WaterMonitorDo/WaterMonitorDo.ino
@@ -1,7 +1,7 @@
 /*********************************************************************
 * WaterMonitor.ino
 *
-* Copyright (C)    2017   [DFRobot](http://www.dfrobot.com)
+* Copyright (C)    2019   [DFRobot](http://www.dfrobot.com)
 * GitHub Link :https://github.com/DFRobot/watermonitor
 * This Library is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -12,14 +12,14 @@
 * This sample code is mainly used to monitor water quality
 * including ph, temperature, dissolved oxygen, ec and orp,etc.
 *
-* Software Environment: Arduino IDE 1.8.2
+* Software Environment: Arduino IDE 1.8.9
 * Software download link: https://www.arduino.cc/en/Main/Software
 *
 * Install the library file：
 * Copy the files from the github repository folder libraries to the libraries
-* in the Arduino IDE 1.8.2 installation directory
+* in the Arduino IDE 1.8.9 installation directory
 *
-* Hardware platform   : Arduino M0 Or Arduino Mega2560
+* Hardware platform   : Teensy 3.6
 * Sensor pin:
 * EC  : A1
 * PH  : A2
@@ -29,14 +29,11 @@
 * GravityDO：A4
 * temperature:D5
 *
-* SD card attached to SPI bus as follows:
-* Mega:  MOSI - pin 51, MISO - pin 50, CLK - pin 52, CS - pin 53
-* and pin #53 (SS) must be an output
-* M0:   Onboard SPI pin,CS - pin 4 (CS pin can be changed)
+* System will contact via MQTT a mosquitto platform to publish data in JSON format.
 *
-* author  :  Jason(jason.ling@dfrobot.com)
+* author  :  Rominco(rtourte@yahoo.fr)
 * version :  V1.0
-* date    :  2017-04-06
+* date    :  2019-09-23
 **********************************************************************/
 
 #include <SPI.h>
