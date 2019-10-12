d61058419a65f9589928bc27efa0dc6cc680d688
AdrienViacesi <33282364+AdrienViacesi@users.noreply.github.com>
2019-10-12 23:33:58 +0200

    Update deploiement.sh

--- systools/deploiement.sh
+++ systools/deploiement.sh
@@ -1,24 +1,24 @@
 ### Variabiliser l'utilisateur
-utilisateur=$USERNAME
+utilisateur=pirate
 
 ### Installation de Node.js et npm
 # Installation de Node.js
-sudo wget https://nodejs.org/dist/v8.9.0/node-v8.9.0-linux-armv6l.tar.gz
-sudo tar -xzf node-v8.9.0-linux-armv6l.tar.gz
+wget https://nodejs.org/dist/v8.9.0/node-v8.9.0-linux-armv6l.tar.gz
+tar -xzf node-v8.9.0-linux-armv6l.tar.gz
 cd node-v8.9.0-linux-armv6l/
 sudo cp -R * /usr/local/
 # Installation des librairies npm
-sudo apt-get install make gcc g++
+apt-get install make gcc g++
 
 ### Création d'un dossier OceanIsOpen et récupération du GitHub
 # Création du dossier OceanIsOpen
-sudo mkdir OceanIsOpen
+mkdir OceanIsOpen
 # Clonage du GitHub dans le dossier
-cd OceanIsOpen && sudo git clone https://github.com/TamataOcean/OceanIsOpen.git
+cd OceanIsOpen && git clone https://github.com/TamataOcean/OceanIsOpen.git
 
 ### Configuration des librairies SaveData et automatisation du démarrage des services au démarrage du Raspberry
 # Création du service oceanisopen
-cd /etc/systemd/system && sudo touch oceanisopen.service
+cd /etc/systemd/system && touch oceanisopen.service
 # Configuration des librairies
 echo "[Unit]
 Description=OceanIsOpen
