f717e9b6ef1f2dee45e5b2686d27031221413c20
rtourte@yahoo.fr <_Corwin12>
2019-10-25 17:06:20 +0200

    Update design for Acquisition

--- react-socket-app/socket-client/src/components/WindowsApp/WAcquire.js
+++ react-socket-app/socket-client/src/components/WindowsApp/WAcquire.js
@@ -5,7 +5,9 @@ import { Connector } from 'mqtt-react';
 import MqttConsole from '../Mqtt/MqttConsole';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import jsonConfig from "./../System/config.json"; 
+import jsonConfig from "./../System/config.json";
+
+import ReactJson from 'react-json-view'
 const listSensors = jsonConfig.sensors;
 
 class WAcquire extends Component {
@@ -36,7 +38,8 @@ class WAcquire extends Component {
         <div className="WAcquire">
           <h2>Acquisition - Harvest Data</h2>
           <h3> Sensors List : Ph - Temperature - Redox - ORP - Oxygen </h3>
-          <Button onClick={this.handleClick} variant="warning" color={this.state.color}>Load Acquisition {this.state.isToggleOn ? 'ON' : 'OFF'} </Button>
+          {/* <ReactJson src={ listSensors }/> */}
+          <Button className="button" onClick={this.handleClick} variant="warning" color={this.state.color}>Load Acquisition {this.state.isToggleOn ? 'ON' : 'OFF'} </Button>
           <Connector mqttProps="ws://192.168.0.104:9001/"> 
             <MqttConsole /> 
           </Connector>
