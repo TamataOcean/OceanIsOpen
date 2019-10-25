67ffb57f901b58932fff6950b0e5bae175878c71
rtourte@yahoo.fr <_Corwin12>
2019-10-25 17:30:29 +0200

    Add Acquisition interval select

--- react-socket-app/socket-client/src/components/WindowsApp/WAcquire.js
+++ react-socket-app/socket-client/src/components/WindowsApp/WAcquire.js
@@ -1,3 +1,4 @@
+import Select from 'react-select';
 import React, {Component, useState} from 'react';
 import './WindowsApp.css';
 
@@ -10,6 +11,15 @@ import jsonConfig from "./../System/config.json";
 import ReactJson from 'react-json-view'
 const listSensors = jsonConfig.sensors;
 
+
+const options = [
+  { value: '5sec', label: 'Every 5sec' },
+  { value: '10sec', label: 'Every 10sec' },
+  { value: '1min', label: 'Every minute' },
+  { value: '1hour', label: 'Every hour' },
+  { value: '1day', label: 'Every day' },
+];
+
 class WAcquire extends Component {
   constructor(props) {
     super(props);
@@ -17,6 +27,16 @@ class WAcquire extends Component {
     this.state = {isToggleOn: true};
     this.handleClick = this.handleClick.bind(this);
   }
+
+  state = {
+    selectedOption: null,
+  };
+  handleChange = selectedOption => {
+    this.setState(
+      { selectedOption },
+      () => console.log(`Option selected:`, this.state.selectedOption)
+    );
+  };
 
   handleClick() {
     console.log("Click...")
@@ -34,15 +54,23 @@ class WAcquire extends Component {
   }
 
   render(){
+      const { selectedOption } = this.state;
       return (
         <div className="WAcquire">
           <h2>Acquisition - Harvest Data</h2>
           <h3> Sensors List : Ph - Temperature - Redox - ORP - Oxygen </h3>
           {/* <ReactJson src={ listSensors }/> */}
           <Button className="button" onClick={this.handleClick} variant="warning" color={this.state.color}>Load Acquisition {this.state.isToggleOn ? 'ON' : 'OFF'} </Button>
+          {/* <h3> Console Log</h3> */}
           <Connector mqttProps="ws://192.168.0.104:9001/"> 
             <MqttConsole /> 
           </Connector>
+          <Select
+        value={selectedOption}
+        onChange={this.handleChange}
+        options={options}
+      />
+
         </div>
         );
   }
