520229bc5fc46b900a1ecd24598c4917e973fd4b
Justin Guerré <guerrejustin@gmail.com>
2019-11-11 15:33:51 +0100

    formatting

--- react-socket-app/server.js
+++ react-socket-app/server.js
@@ -1,43 +1,42 @@
-const express = require('express')
-const http = require('http')
-const socketIO = require('socket.io')
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
 
 // our localhost port
-const port = 4001
+const port = 4001;
 
-const app = express()
+const app = express();
 
 // our server instance
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 // This creates our socket using the instance of the server
-const io = socketIO(server)
+const io = socketIO(server);
 
 // This is what the socket.io syntax is like, we will work this later
-io.on('connection', socket => {
-  console.log('New client connected')
-  
-  // just like on the client side, we have a socket.on method that takes a callback function
-  socket.on('change color', (color) => {
-    // once we get a 'change color' event from one of our clients, we will send it to the rest of the clients
-    // we make use of the socket.emit method again with the argument given to use from the callback function above
-    console.log('Color Changed to: ', color)
-    io.sockets.emit('change color', color)
-  })
-  
-  // just like on the client side, we have a socket.on method that takes a callback function
-  socket.on('calibrate ph', () => {
-    console.log('Calibrate ph asked')
-    
-    // Emit to Teensy to be prepared.
-    io.sockets.emit('calibrate_ph_1')
-  })
-  
+io.on("connection", socket => {
+    console.log("New client connected");
 
-  // disconnect is fired when a client leaves the server
-  socket.on('disconnect', () => {
-    console.log('user disconnected')
-  })
-})
+    // just like on the client side, we have a socket.on method that takes a callback function
+    socket.on("change color", color => {
+        // once we get a 'change color' event from one of our clients, we will send it to the rest of the clients
+        // we make use of the socket.emit method again with the argument given to use from the callback function above
+        console.log("Color Changed to: ", color);
+        io.sockets.emit("change color", color);
+    });
 
-server.listen(port, () => console.log(`Listening on port ${port}`))
+    // just like on the client side, we have a socket.on method that takes a callback function
+    socket.on("calibrate ph", () => {
+        console.log("Calibrate ph asked");
+
+        // Emit to Teensy to be prepared.
+        io.sockets.emit("calibrate_ph_1");
+    });
+
+    // disconnect is fired when a client leaves the server
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
