c1e902ceed790c1e8bbbadd042a47e4c7b044813
contact@tamataocean.com <_Corwin12>
2020-07-27 09:52:48 +0200

    Version for Serial Communication

--- teensy/teensy.ino
+++ teensy/teensy.ino
@@ -29,16 +29,16 @@
 * GravityDO：A4
 * temperature:D5
 *
-* System will contact via MQTT a mosquitto platform to publish data in JSON format.
+* System will contact via Serial USB to publish data in JSON format.
 *
 * author  :  Rominco(rtourte@yahoo.fr)
-* version :  V1.0
-* date    :  2019-09-23
+* version :  V1.1
+* date    :  2020-07-27
 **********************************************************************/
 
 #include <SPI.h>
-#include <Ethernet.h>
-#include <PubSubClient.h>
+//#include <Ethernet.h>
+//#include <PubSubClient.h>
 //#include "NetworkControl.h"
 
 #include <SD.h>
@@ -51,68 +51,15 @@
 #include "SdService.h"
 #include "Debug.h"
 
-/* SOCKET IO --------------- 
-#include <WebSocketClient.h>
-char server[] = "192.168.0.104";
-WebSocketClient clientWS; 
-*/
+//Create variable to track time
+unsigned long updateTime = 0;
+unsigned long logInterval = 10000;
+unsigned long previousLogTime = 0;
 
 // Alias sensor logic as sensorHub 
 GravitySensorHub sensorHub ;
-
-/* NETWORK & MQTT Config */
-/*************************/
-//NetworkControl netControl = NetworkControl(sensorHub.sensors);
-//byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
-byte mac[] = {   0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
-IPAddress ip(192, 168, 0, 5);
-
-IPAddress serverMqtt(192, 168, 0, 4);
-char* outTopic = "teensy/sensors";
-char* outTopicLog = "teensy/console";
-
-char* inTopicOrder = "teensy/order";
-char* outTopicOrder = "teensy/check";
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-
-  String order((char *) payload );
-  Serial.println( order);
-  
-  if ( order.equals((String)"phCalibrate") ) {
-    Serial.println("PhCalibrate payload...");
-    }
-    else Serial.println("payload pas compris...");
-}
-
-EthernetClient ethClient;
-PubSubClient client(serverMqtt, 1883, callback, ethClient);
-
-void reconnect(int nbTry) {
-  int cpt = 0;
-  // Loop until we're reconnected
-  while (!client.connected() && cpt < nbTry ) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect(serverMqtt)) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish(outTopicLog,sensorHub.getJsonSensorsUpdate().c_str());
-      // ... and resubscribe
-      client.subscribe(inTopicOrder);
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try "+ (String)cpt + "/"+ (String)nbTry +". Next try in 5 seconds ");
-      // Wait 5 seconds before retrying
-      delay(5000);
-      cpt++;
-    }
-  }
-}
+String name = "TEENSY-SERIAL";
+int start_log = 0;
 
 /*************************/
 /*        SETUP          */
@@ -121,47 +68,10 @@ void setup() {
 	//SERIAL INIT
 	Serial.begin(9600);
 	delay(1000);
-	Debug::println("Serial begin");
+	Debug::println("INIT_SENSOR_HUB");
 	
-	/********************************/
-	/* 			NETWORK SETUP 		*/
-	/*								*/	
-  Debug::println("Initialize Ethernet with DHCP:");
-  /* Static Ip Config */
-  Ethernet.begin(mac, ip);
-  
-  /* DHCP config */
-  /* if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-    } else if (Ethernet.linkStatus() == LinkOFF) {
-      Serial.println("Ethernet cable is not connected.");
-    }
-    // no point in carrying on, so do nothing forevermore:
-    while (true) {
-      delay(1);
-    }
-  }
-  */
-  // print your local IP address:
-  Serial.print("My IP address: ");
-  Serial.println(Ethernet.localIP());
-	delay(1500);						  	// Allow the hardware to sort itself out
-
-  /********************************/
-  /* MQTT                         */
-  /*                              */
-  if (client.connect("arduinoClient", "testuser", "testpass")) {
-    client.publish(outTopicLog,"Teensy arrived...");
-    client.subscribe( inTopicOrder );
-  }
-   client.setServer(serverMqtt, 1883);
-   client.setCallback(callback);
-  
-	/********************************/
+	/**************************/
 	/* 			SENSORS SETUP 		*/
-	/*								*/
   //Reset and initialize sensors
 	Debug::println("... SensorHub setup begin...");
 	sensorHub.setup();
@@ -171,41 +81,56 @@ void setup() {
 	((GravityPh*)(sensorHub.sensors[phSensor]))->setOffset(PHOFFSET);
 	Debug::print("pH offset: ");
 	Debug::println(PHOFFSET);
-	
-  /********************************/
-  /*      SOCKET IO               */
-  /*                              
-	clientWS.connect(serverMqtt);
-  if (clientWS.connected()) Serial.println("connected to WebSocket");
-  else Serial.println("WS not connected");*/
 }
-
-//Create variable to track time
-unsigned long updateTime = 0;
-unsigned long logInterval = 10000;
-unsigned long previousLogTime = 0;
 
 /*************************/
 /*        LOOP           */
 /*************************/
 void loop() {
-	//MQTT Connection 
-	if (!client.connected()) {
-        Serial.println("MQTT not connected");
-      	reconnect(5);
-  }
 
-  client.loop();
-
-  if (  (millis() - previousLogTime) >= logInterval || previousLogTime == 0 ) {
+  if (  ((millis() - previousLogTime) >= logInterval || previousLogTime == 0 ) && start_log ) {
     //Collect sensor readings
     sensorHub.update();
     //Export sensor in JSON
-    Serial.println("Sending to MQTT Topic : " + (String)MQTTTOPIC);
     Serial.println(sensorHub.getJsonSensorsUpdate().c_str());
-    client.publish(outTopic,sensorHub.getJsonSensorsUpdate().c_str());
-    Serial.println("MQTT message sent");
     previousLogTime = millis(); 
     }
 
+  if (Serial.available() > 0) {
+    String data = Serial.readStringUntil('\n');
+    Serial.print( name + " - message received : ");
+    Serial.println(data);
+    commandManager(data);
+  }  
+}
+
+// Management Command order
+int commandManager(String message) {
+
+  if (message == "Init_connection_from_Raspi") {
+    Serial.println( name + " - INIT Raspi received ");
+    Serial.print( "config start_log = " );
+    Serial.println( start_log );
+  }
+  else if (message == "restart") {
+    Serial.println( name + " - RESTART in progress ");
+    _reboot_Teensyduino_();
+    //ESP.restart();
+  }
+  else if (message == "calibrate") {
+    Serial.println( name + " - CALIBRATE in progress ");
+  }
+  else if (message == "startLog") {
+    Serial.println( name + " - Start log received ");
+    start_log = 1;
+  }
+
+  else if (message == "stopLog") {
+    Serial.println( name + " - StopLog received");
+    start_log = 0;
+  }
+
+  else {
+    Serial.println( "Unknown command : " + message );
+  }
 }
