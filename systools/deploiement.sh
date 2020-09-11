d8b8d3f50f429af7e3b3cd9da30bbed133c14026
ANCELIN Julien <julien.ancelin@inra.fr>
2020-09-12 01:05:32 +0200

    -O

--- systools/deploiement.sh
+++ systools/deploiement.sh
@@ -3,7 +3,7 @@ utilisateur=pi
 
 ### Installation de Node.js et npm
 # Installation de Node.js
-wget https://nodejs.org/dist/v8.9.0/node-v8.9.0-linux-armv6l.tar.gz
+wget -O https://nodejs.org/dist/v8.9.0/node-v8.9.0-linux-armv6l.tar.gz
 tar -xzf node-v8.9.0-linux-armv6l.tar.gz
 cd node-v8.9.0-linux-armv6l/
 sudo cp -R * /usr/local/
