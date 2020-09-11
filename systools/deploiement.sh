39525326d84fb48b34b26867398900aaef083873
Romain Tourte <rtourte@yahoo.fr>
2020-09-12 00:46:16 +0200

    Update deploiement.sh

--- systools/deploiement.sh
+++ systools/deploiement.sh
@@ -1,5 +1,5 @@
 ### Variabiliser l'utilisateur
-utilisateur=VOTRE_NOM_D'UTILISATEUR
+utilisateur=pi
 
 ### Installation de Node.js et npm
 # Installation de Node.js
@@ -9,6 +9,16 @@ cd node-v8.9.0-linux-armv6l/
 sudo cp -R * /usr/local/
 # Installation des librairies npm
 apt-get install make gcc g++
+
+# InfluxDB install
+sudo apt install -y gnupg2 curl wget
+wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+echo "deb https://repos.influxdata.com/debian buster stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+sudo apt install -y influxdb
+sudo systemctl enable --now influxdb
+ 
+### Create database
+influx -execute 'create database datateensy'
 
 ### Création d'un dossier OceanIsOpen et récupération du GitHub
 # Création du dossier OceanIsOpen
