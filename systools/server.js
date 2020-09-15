905fb6cb5ddaeb06070fd42300c55c236a76b58e
rtourte@yahoo.fr <_Corwin12>
2020-09-15 16:22:31 +0200

    Update retro code to get start/stop log

--- systools/server.js
+++ systools/server.js
@@ -231,6 +231,24 @@ function begin() {
 		})
 	})
 
+	.post('/api/command', ( req, res ) => {
+		console.log('API Command requested with POST Method: ' + req.query.cmd_id);
+		console.log('Command requested : '+ "{\"order\":\"" + req.query.cmd_id + "\"}" );
+		console.log('Interval requested : '+ "{\"interval\":\"" + req.query.interval + "\"}" );
+		
+       	port_TEENSY.write("{\"order\":\"" + req.query.cmd_id + "\"}" , function(err){
+			if (err) {
+				return console.log('Error : ', err.message);
+       	   	}
+          	console.log('command ' + req.query.cmd_id + ' sent');
+           	//res.redirect('/');
+		   })
+		   
+		console.log(req.body);
+		console.log("Interval : ", req.interval);
+		res.send(`Server received your POST request. This : ${req.body.post}`);
+	})
+
 	.post('/api/updateLogInterval', ( req, res ) => {
 		console.log('Interval requested : '+ "{\"interval\":\"" + req.query.interval + "\"}" );
 		
