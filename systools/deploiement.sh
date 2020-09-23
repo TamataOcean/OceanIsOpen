d3ca0903b016939a9aeb33b51bc5fd4686186513
Romain Tourte <rtourte@yahoo.fr>
2020-09-23 14:53:12 +0200

    Update deploiement.sh

--- systools/deploiement.sh
+++ systools/deploiement.sh
@@ -1,3 +1,19 @@
+### TEST avec nouvelle install
+
+#### GRAFANA
+
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update
+sudo apt-get install -y grafana
+
+#### NODEJS
+
+
+#### OIO PACKAGE
+
+
+
 ### Variabiliser l'utilisateur
 utilisateur=pi
 
