9249eb298cacc1f8f5ba3f342f95d7db854b8e11
rtourte@yahoo.fr <_Corwin12>
2020-12-07 16:39:00 +0100

    update GNSS Json format

--- systools/actions/components/TamataPostgres.js
+++ systools/actions/components/TamataPostgres.js
@@ -72,77 +72,37 @@ class TamataPostgres {
          // body...
          } 
       else  {  
-         if (GPS_Modele == "standard") {
-            /* USB GPS Classic Format */
-            console.log("     datetime = " + JSON.stringify(jsonPosition.datetime) );
-            console.log("     coordinates[0] = " + jsonPosition.loc.geojson.coordinates[0]);
-            console.log("     coordinates[1] = " + jsonPosition.loc.geojson.coordinates[1] );
-            console.log("     latitude = " + jsonPosition.loc.dmm.latitude );
-            console.log("     longitude = " + jsonPosition.loc.dmm.longitude );
-            console.log("     speed= " + jsonPosition.speed.knots );
-            
-            if (DEBUG) console.log('----------- QUERY for GPS Modele Standard ----------------------------------------');     
-            const queryText = "INSERT INTO sensors(\"teensy_user\", \"teensy_phsensor\", \"teensy_temperaturesensor\", \"teensy_dosensor\", \"teensy_ecsensor\", \"teensy_tdssensor\", \"teensy_orpsensor\","+
-            "\"gnss_modele\", \"gnss_datetime\", \"nmea_latitude\", \"nmea_longitude\", \"nmea_speed\" ) VALUES('"+
-            jsonRecord.state.reported.user +"'," +          //FOR TEXT Value have to be 'VALUE'
-            // phSensor  
-            jsonRecord.state.reported.sensors[0].value + ","+ 
-            // temperatureSensor + ","+ 
-            jsonRecord.state.reported.sensors[1].value + ","+ 
-            // doSensor + ","+ 
-            jsonRecord.state.reported.sensors[2].value + ","+ 
-            // ecSensor + ","+ 
-            jsonRecord.state.reported.sensors[3].value + ","+ 
-            // tdsSensor + ","+ 
-            jsonRecord.state.reported.sensors[4].value + ","+ 
-            // orpSensor + ",'" +
-            jsonRecord.state.reported.sensors[5].value + ",'" +
-            
-            /* USB GPS Classic */
-            GPS_Modele + "','" +
-            JSON.stringify(jsonPosition.datetime) + "'," +
-            jsonPosition.loc.geojson.coordinates[1] + "," +
-            jsonPosition.loc.geojson.coordinates[0] +"," +
-            jsonPosition.speed.knots + ")";
-            
-            if (DEBUG) console.log("queryText " + queryText);
-            this.pool.query(queryText, (err, res) => {
-               console.log(err, res);
-            }); 
-         }
+         /* Drotek & Standard Format */
+         console.log("     datetime = " + jsonPosition.gps.datetime );
+         console.log("     latitude = " + jsonPosition.geo.latitude );
+         console.log("     longitude = " + jsonPosition.geo.longitude );
+         console.log("     speed= " + jsonPosition.speed.knots );
+      
+         if (DEBUG) console.log('(----------- QUERY for  Modele Drotek ----------------------------------------');     
+         const queryText = "INSERT INTO sensors(\"teensy_user\", \"teensy_phsensor\", \"teensy_temperaturesensor\", \"teensy_dosensor\", \"teensy_ecsensor\", \"teensy_tdssensor\", \"teensy_orpsensor\","+
+         "\"gnss_modele\",\"gnss_datetime\", \"nmea_latitude\", \"nmea_longitude\", \"nmea_speed\" ) VALUES('"+
+         jsonRecord.state.reported.user +"'," +          //FOR TEXT Value have to be 'VALUE'
+         jsonRecord.state.reported.sensors[0].value + ","+ 
+         jsonRecord.state.reported.sensors[1].value + ","+ 
+         jsonRecord.state.reported.sensors[2].value + ","+ 
+         jsonRecord.state.reported.sensors[3].value + ","+ 
+         jsonRecord.state.reported.sensors[4].value + ","+ 
+         jsonRecord.state.reported.sensors[5].value + ",'" +
          
-         else if ( GPS_Modele == "Drotek") {
-         /* Drotek Format */
-            console.log("     datetime = " + jsonPosition.gps.datetime );
-            console.log("     latitude = " + jsonPosition.geo.latitude );
-            console.log("     longitude = " + jsonPosition.geo.longitude );
-            console.log("     speed= " + jsonPosition.speed.knots );
+         /* GPS data */
+         GPS_Modele + "','" +
+         JSON.stringify(jsonPosition.gps.datetime)  + "'," +
+         jsonPosition.geo.latitude + "," +
+         jsonPosition.geo.longitude +"," +
+         jsonPosition.speed.knots + ")";
          
-            if (DEBUG) console.log('(----------- QUERY for  Modele Drotek ----------------------------------------');     
-            const queryText = "INSERT INTO sensors(\"teensy_user\", \"teensy_phsensor\", \"teensy_temperaturesensor\", \"teensy_dosensor\", \"teensy_ecsensor\", \"teensy_tdssensor\", \"teensy_orpsensor\","+
-            "\"gnss_modele\",\"gnss_datetime\", \"nmea_latitude\", \"nmea_longitude\", \"nmea_speed\" ) VALUES('"+
-            jsonRecord.state.reported.user +"'," +          //FOR TEXT Value have to be 'VALUE'
-            jsonRecord.state.reported.sensors[0].value + ","+ 
-            jsonRecord.state.reported.sensors[1].value + ","+ 
-            jsonRecord.state.reported.sensors[2].value + ","+ 
-            jsonRecord.state.reported.sensors[3].value + ","+ 
-            jsonRecord.state.reported.sensors[4].value + ","+ 
-            jsonRecord.state.reported.sensors[5].value + ",'" +
-            
-            /* GPS data */
-            GPS_Modele + "','" +
-            JSON.stringify(jsonPosition.gps.datetime)  + "'," +
-            jsonPosition.geo.latitude + "," +
-            jsonPosition.geo.longitude +"," +
-            jsonPosition.speed.knots + ")";
-            
-            if (DEBUG) console.log("queryText " + queryText);
-            this.pool.query(queryText, (err, res) => {
-               console.log(err, res);
-               //pool.end();
-            }); 
-            // body...
-         }
+         if (DEBUG) console.log("queryText " + queryText);
+         this.pool.query(queryText, (err, res) => {
+            console.log(err, res);
+            //pool.end();
+         }); 
+         // body...
+         
    }
    }
 }
