5b774bd21a4774759dc1a3b2ce22f088b32c2974
Romain Tourte <rtourte@yahoo.fr>
2023-04-13 14:43:05 +0200

    Update deploy.sh

--- systools/deploy.sh
+++ systools/deploy.sh
@@ -1,13 +1,12 @@
 ### TEST avec nouvelle install
-#### BASICS
+## BASICS
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install -y git
 
-
-#### INSTALL Mosquitto
+## INSTALL Mosquitto
 sudo apt install -y mosquitto mosquitto-clients
 
-#### GRAFANA
+## GRAFANA
 
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
@@ -17,9 +16,17 @@ sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable grafana-server
 sudo /bin/systemctl start grafana-server
 
-#### NODEJS
+## NODEJS
 curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
 sudo apt-get install -y nodejs
+
+## INFLUXDB Install
+wget -q https://repos.influxdata.com/influxdata-archive_compat.key
+echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
+sudo apt-get update && sudo apt-get install influxdb2
+
+# Create Influx DATABASE
 
 #### OIO PACKAGE
 cd /home/pi
